cc704367af37b1d4261170c8d269d79bc74cfbe7
hskuntze <hskuntze@gmail.com>
2023-01-17 18:32:14 -0300

    Ready to start

---
+++ src/Routes.js
@@ -0,0 +1,19 @@
+import Home from "pages/Home";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes as Switch,
+} from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <Router>
+      {/* <Navbar /> */}
+      <Switch>
+        <Route path="/" element={<Home />} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const Home = () => {
+    return (
+        <main id="home-content">
+            <div>Home</div>
+        </main>
+    );
+}
+
+export default Home;
